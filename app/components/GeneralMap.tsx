d4b9f81eec1aa2c8b238c087d3c91db3ca98dace
mkettel <mattkettelkamp@gmail.com>
2024-09-02 23:46:53 -0400

    updated some stuff

--- app/components/GeneralMap.tsx
+++ app/components/GeneralMap.tsx
@@ -2,7 +2,10 @@
 
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
+
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY as string;
 
@@ -33,13 +36,44 @@ const Map: React.FC = () => {
         'tileSize': 512,
         'maxzoom': 14
       });
+
       map.current.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+
+      // Add the geocoder control to the map
+      const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        marker: false,
+        placeholder: 'Search for a location',
+      });
+
+      map.current.addControl(geocoder);
+
+      // Listen for the 'result' event from the Geocoder
+      geocoder.on('result', (e) => {
+        const coords = e.result.center;
+        map.current?.flyTo({
+          center: coords,
+          essential: true,
+          zoom: 12 // You can adjust this zoom level
+        });
+      });
     });
-  });
+
+    // Update state when the map moves
+    map.current.on('move', () => {
+      if (!map.current) return;
+      setLng(Number(map.current.getCenter().lng.toFixed(4)));
+      setLat(Number(map.current.getCenter().lat.toFixed(4)));
+      setZoom(Number(map.current.getZoom().toFixed(2)));
+    });
+  }, []);
 
   return (
     <div>
-      <div ref={mapContainer} className="map-container rounded-md" style={{ height: '800px', width: '900px' }} />
+      <div className="sidebar my-2">
+        <span>Longitude:</span> <span className='px-2 text-sm  text-white bg-black border-black border-2 rounded-full'>{lng}</span> | Latitude: <span className='px-2 text-sm text-white bg-black border-black border-2 rounded-full'>{lat}</span> | Zoom: <span className='px-2 text-sm text-white bg-black border-black border-2 rounded-full'>{zoom}</span>
+      </div>
+      <div ref={mapContainer} className="map-container rounded-md" style={{ height: '700px', width: '800px' }} />
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
-import Map from "./components/GeneralMap";
+// import Map from "./components/GeneralMap";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("./components/GeneralMap"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false
+});
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-start p-6  ">
       <h1 className="text-3xl font-bold text-center mb-6">SkiAR</h1>
-      <div className="flex flex-col w-full items-center justify-between">
+      <div className="flex relative flex-col w-full items-center justify-between">
         <Map />
       </div>
     </main>
